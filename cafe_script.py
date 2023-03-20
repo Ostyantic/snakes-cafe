24455ca80bffe53e73a48c25a7da0c9c427e3e53
Anthony Lopez <Antl21292@gmail.com>
2023-03-20 18:15:39 -0400

    Refactored code

--- cafe_script.py
+++ cafe_script.py
@@ -35,34 +35,23 @@ Unicorn Tears"""
     print(menu)
 
     Menu = {
-        "Appetizer": ["wings", "cookies", "spring rolls"],
-        "Entrees": ["salmon", "steak", "meat tornado", "a literal garden"],
-        "Dessert": ["ice cream", "cake", "pie"],
-        "Drinks": ["coffee", "tea", "unicorn tears"],
+        "wings": 0,
+        "cookies": 0,
+        "spring rolls": 0,
+        "salmon": 0,
+        "steak": 0,
+        "meat tornado": 0,
+        "a literal garden": 0,
+        "ice cream": 0,
+        "cake": 0,
+        "pie": 0,
+        "coffee": 0,
+        "tea": 0,
+        "unicorn tears": 0,
     }
 
-    user_selection = {
-        "Appetizer": {
-            "wings": 0,
-            "cookies": 0,
-            "spring rolls": 0
-        },
-        "Entrees": {
-            "salmon": 0,
-            "steak": 0,
-            "meat tornado": 0,
-            "a literal garden": 0
-        },
-        "Dessert": {
-            "ice cream": 0,
-            "cake": 0,
-            "pie": 0
-        },
-        "Drinks": {
-            "coffee": 0,
-            "tea": 0,
-            "unicorn tears": 0
-        },
+    customer_order = {
+
     }
 
     order = input("""***********************************
@@ -72,13 +61,18 @@ Unicorn Tears"""
 
     while order != "quit":
         # print(Menu.keys())
-        for course in Menu:
-            if order.lower() in Menu[course]:
-                user_selection[course][order.lower()] += 1
-                order = input(f"""You have ordered {user_selection[course][order.lower()]} order(s) of {order}. 
-Anything else? """)
+        if order.lower() in Menu:
 
-        order = input(f"{order} doesn't exist in our menu, please choose another item. ")
+            # Menu[order.lower()] += 1
+            if order.lower() not in customer_order:
+                customer_order[order.lower()] = 1
+            else:
+                customer_order[order.lower()] += 1
+            print(customer_order)
+            order = input(f"""You have ordered {customer_order[order.lower()]} order(s) of {order}. 
+Anything else? """)
+        else:
+            order = input(f"{order} doesn't exist in our menu, please choose another item. ")
 
         # for item_group in Menu.values():
         #     print(item_group)
